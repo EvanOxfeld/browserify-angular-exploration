eda4c973308c8df7ddb57eb97eca1bd7529e339a
Evan Oxfeld <eoxfeld@gmail.com>
2014-08-08 22:01:09 -0400

    Extract app routes into separate file

--- app/app.js
+++ app/app.js
@@ -4,32 +4,5 @@ angular
   .module('myTodos', [
     require('angular-ui-router')
   ])
-  .config(function($stateProvider) {
-    $stateProvider
-      .state('index', {
-        url: '',
-        views: {
-          'sidebarView': {
-            template: '<ol><li ng-repeat="todo in todos.list"><a ng-click="todos.active = todo">{{ todo.name }}</a>' +
-                     ' [<a href ng-click="todos.delete($index)">X</a>]</li></ol>' +
-                      '<h3 ng-hide="todos.list.length > 0">No Todos</h3>'
-          },
-          'mainView': {
-            template: [
-              '<div ng-show="todos.active">',
-              'Name: <input ng-model="todos.active.name"/>',
-              '<h4>Description:</h4>',
-              '<textarea rows="20" ng-model="todos.active.description"></textarea>',
-              '</div>',
-              '<div ng-hide="todos.active"><-- Select a todo</div>'
-            ].join('')
-          }
-        }
-      });
-  });
-
-var todoController = require('./controller');
-
-angular
-  .module('myTodos')
-  .controller('TodoController', todoController);
+  .config(require('./routes'))
+  .controller('TodoController', require('./controller'));
